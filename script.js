520739e064e8c2937bfcfa7c58799e7816fbe751
Brad Culwell <beastwick18@gmail.com>
2023-02-23 00:01:40 -0600

    02-22-23 12:59:07

---
+++ script.js
@@ -0,0 +1,63 @@
+$('.about-me-button').click(() => {
+    var element = document.getElementById('about-me');
+    element.scrollIntoView();
+});
+$('.projects-button').click(() => {
+    var element = document.getElementById('projects');
+    element.scrollIntoView();
+});
+
+// This is incredibly stupid, but it works
+var current = 0;
+var max_elem = document.querySelectorAll('.background').length;
+var mode = 0;
+setInterval(() => {
+    var items = document.querySelectorAll('.background.ready');
+    console.log(current);
+    items.forEach((elem) => {
+        if($(elem).data('index') == current) {
+            // Reset all z index
+            if(current == 0) {
+                items.forEach((elem) => {
+                    $(elem).css('z-index', '');
+                });
+            }
+            $(elem).css('transition', '');
+            $(elem).removeClass('ready');
+            if(mode == 0)
+                $(elem).addClass('animate-left');
+            else if(mode == 1)
+                $(elem).addClass('animate-right');
+            else if(mode == 2)
+                $(elem).addClass('animate-up');
+            else if(mode == 3)
+                $(elem).addClass('animate-down');
+            mode = (mode + 1) % 4;
+            setTimeout(() => {
+                $(elem).css('transition', 'all 0s');
+                $(elem).css('z-index', '-1');
+                $(elem).removeClass('animate-left');
+                $(elem).removeClass('animate-right');
+                $(elem).removeClass('animate-up');
+                $(elem).removeClass('animate-down');
+                $(elem).addClass('ready');
+                current = (current + 1) % max_elem;
+            }, 1000);
+        }
+    });
+}, 5000);
+
+var btn = $('#totop');
+
+$(window).scroll(() => {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.click((e) => {
+    e.preventDefault();
+    $('html, body').stop(true, true).animate({scrollTop:0}, 100);
+});
