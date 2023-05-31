def385b9b0f0a2e4052cbfc3f3d888a6f63922ec
Brad Culwell <beastwick18@gmail.com>
2023-05-31 00:28:32 -0500

    05-30-23 23:26:09

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // This is incredibly stupid, but it works
 var current = 0;
@@ -51,8 +51,8 @@ $(window).scroll(() => {
 //     e.preventDefault();
 // });
 
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    vh = Math.max(window.innerHeight * 0.01, vh);
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     vh = Math.max(window.innerHeight * 0.01, vh);
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
